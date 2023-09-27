8e2b6f0f34b4272aa97a4a0561143f4b7937fa5d
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-27 08:43:14 -0400

    added functions to js to collect display and store the current data

---
+++ script.js
@@ -0,0 +1,19 @@
+const apiKey = 'YOUR_API_KEY';
+function fetchWeatherData(city) {
+    // place older to get the data  we need some type of function
+}
+
+//place older to  display current weather data 
+function displayCurrentWeather(data) {
+   
+}
+
+// Function to display 5-day forecast data
+function displayForecast(data) {
+    
+}
+
+// Function to add a city to the search history
+function addToSearchHistory(city) {
+   //  placeholder to display and store the data that we searchedd
+}
